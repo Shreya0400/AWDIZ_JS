a613122baf001ad7488ddfd060e535219f577c57
Shreya0400 <79578221+Shreya0400@users.noreply.github.com>
2024-04-19 20:13:47 +0530

    Day3 updates

---
+++ Day1/index.js
@@ -0,0 +1,7 @@
+var myStudentName = "shreya" 
+var myStudentAge = 24
+var myStudentCompletedAssignment = true
+
+console.log(myStudentName)
+console.log(myStudentAge)
+console.log(myStudentCompletedAssignment)
